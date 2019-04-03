5d01f3d3b75375b00dd89a2f55ee9d90d8a070f9
sykim <sykim@i-on.net>
2019-04-03 18:18:26 +0900

    라우트 분리

---
+++ server/route/index.js
@@ -0,0 +1,32 @@
+const express = require( 'express' );
+const path = require( 'path' );
+
+const router = express.Router();
+
+router.get( '/test', function( req, res ) {
+  const id = req.query.id;
+  const name = req.query.name;
+  if ( req.session.rds.some( ss => ss.id === id ) ) {
+    res.json( { code: '400', message: 'fail', detail: 'aleady signed' } );
+  } else {
+    req.session.rds.push( { id, name } );
+    res.json( { code: '200', message: 'success' } );
+  }
+} );
+
+router.get( '/users', function( req, res ) {
+  res.json( { session: req.session } );
+} );
+
+router.get( '/users', function( req, res ) {
+  res.json( { session: req.session } );
+} );
+
+router.get( '/logout', function( req, res ) {
+  req.session.destroy( function( err ) {
+    // cannot access session here
+    res.json( { message: 'logout' } );
+  } );
+} );
+
+module.exports = router;
